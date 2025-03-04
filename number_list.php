2ab7b27532b0bc3a910bbb24df02ad8250d2f575
Falcon1851 <oshhepkov.nikolaii@nttek.ru>
2025-03-04 11:28:40 +0500

    123

--- number_list.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$number = 1;
-
-while ($number <= 10) {
-    print("<li>$number</li>");
-    $number++;
-}
--- number_list_do_while.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$number = 1;
-
-do {
-    print("<li>$number</li>");
-    $number++;
-} while ($number <= 10);
